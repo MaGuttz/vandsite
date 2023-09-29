4ec88be42ccbc0d693a6c60fe863218d10dc71c4
ctva <theaberchio@gmail.com>
2023-09-29 16:28:39 +0700

    js: prevent page redirection in gallery confirmation dialog in the previous js commit, it turns out that the code i wrote was less complex which caused a miscommunication where when the user clicks the OK button on the alert() dialog, the browser still opens the 'gallery' page which doesn't actually exist (still in progress)

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ function ygBener() {
     var konfirmasi = confirm("Apakah Anda yakin ingin mengunjungi gallery kami?");
     if (konfirmasi) {
         alert("Maaf, gallery kami sedang dalam perbaikan.");
+        return false;
     }
     return konfirmasi;
 }
